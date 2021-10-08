930122444715e6d7632d67ddcfb1d00e34f1491c
M Iqbal Adenan <kumpultugas17@gmail.com>
2021-10-08 09:49:28 +0800

    Update timeline.php

--- kumpulan_latihan/login-petani-kode/timeline.php
+++ kumpulan_latihan/login-petani-kode/timeline.php
@@ -1,6 +1,5 @@
 <?php require_once("auth.php"); ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,35 +13,24 @@
 </head>
 
 <body class="bg-light">
-
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-4">
-
                 <div class="card">
                     <div class="card-body text-center">
-
                         <img class="img img-responsive rounded-circle mb-3" width="160" src="img/<?php echo $_SESSION['user']['photo'] ?>" />
-
                         <h3><?php echo  $_SESSION["user"]["name"] ?></h3>
                         <p><?php echo $_SESSION["user"]["email"] ?></p>
-
                         <p><a href="logout.php">Logout</a></p>
                     </div>
                 </div>
-
-
             </div>
-
-
             <div class="col-md-8">
-
                 <form action="" method="post" />
                 <div class="form-group">
                     <textarea class="form-control" placeholder="Apa yang kamu pikirkan?"></textarea>
                 </div>
                 </form>
-
                 <?php for ($i = 0; $i < 6; $i++) { ?>
                     <div class="card mb-3">
                         <div class="card-body">
@@ -50,12 +38,9 @@
                         </div>
                     </div>
                 <?php } ?>
-
             </div>
-
         </div>
     </div>
-
 </body>
 
 </html>
